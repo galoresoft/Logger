9ff2af524326d151ee210c7c78d846cb30b23060
Gabriel Mtz Carrillo <gabrielmtzx7@gmail.com>
2018-02-06 12:28:00 -0600

    First

---
+++ Logger/Log.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.CompilerServices;
+
+namespace Galoresoft.Diagnostics
+{
+    public static class Log
+    {
+        //public delegate void CallbackHandler(string message);
+        public static event Action<string> OnLog;
+        
+        public static string FileName { get; set; }
+
+        static void DoLog(string message)
+        {
+            OnLog?.Invoke(message);
+        }
+        
+        static Log()
+        {
+            FileName = null;
+        }
+
+        /// <summary>
+        /// Write raw text, excluding timestamp.
+        /// </summary>
+        /// <param name="message">String.Format-like text.</param>
+        /// <param name="args"></param>
+        public static void Write(string message, params object[] args)
+        {
+            try
+            {
+                string tmpmessage = string.Format(message, args);
+                File.AppendAllText(FileName ?? string.Format("{0:yyyy-MM-dd}.log", DateTime.Now), tmpmessage);
+
+#if DEBUG
+                Debug.WriteLine(tmpmessage);
+#endif
+                OnLog?.Invoke(tmpmessage);
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Write text and it's timestamp, ending with a new line.
+        /// </summary>
+        /// <param name="message">String.Format-like text.</param>
+        /// <param name="args"></param>
+        public static void WriteLine(string message = null, params object[] args)
+        {
+            try
+            {
+                string tmpmessage = string.IsNullOrEmpty(message) ? string.Empty : DateTime.Now.ToString("yyy-MM-dd HH:mm:ss ") + string.Format(message ?? string.Empty, args);
+                File.AppendAllText(FileName ?? string.Format("{0:yyyy-MM-dd}.log", DateTime.Now), tmpmessage + Environment.NewLine);
+
+#if DEBUG
+                Debug.WriteLine(tmpmessage);
+#endif
+                OnLog?.Invoke(tmpmessage);
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ LoggerTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Galoresoft.Diagnostics;
+
+namespace LoggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bind OnLog Event
+            Log.OnLog += Log_OnLog;
+
+            Log.Write("Test A: ({0}", DateTime.Now);
+            Log.Write(")<NewLine>");
+
+            Log.WriteLine();
+            Log.WriteLine("*");
+
+            //Unbind OnLog Event
+            Log.OnLog -= Log_OnLog;
+
+            Log.WriteLine("Test B");
+
+            Console.ReadKey();
+        }
+
+        private static void Log_OnLog(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
